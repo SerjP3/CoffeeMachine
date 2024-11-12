9ce2adc21a7aba5c1141e4fd3476065b593f685f
SerjP3 <sprodan@deloittece.com>
2024-11-12 08:38:17 +0100

    second commit

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ resources = {
 money = 0
 
 def check_resources(drink):
+    """Returns true if there are enough resources to make the drink, or false if not"""
     ingredients = MENU[drink]["ingredients"]
     for item in ingredients:
         if ingredients[item] > resources.get(item, 0):
@@ -50,19 +51,31 @@ def make_drink(drink):
     print(f"Here is your {drink}. Enjoy!")
 
 def print_report():
+    """Prints the current resources"""
     print(f"Water: {resources['water']}ml")
     print(f"Milk: {resources['milk']}ml")
     print(f"Coffee: {resources['coffee']}g")
     print(f"Money: ${money:.2f}")
 
 def process_coins():
+    """Returns the total calculated from coins inserted"""
     quarters = int(input("How many quarters? ")) * 0.25
     dimes = int(input("How many dimes? ")) * 0.10
     nickels = int(input("How many nickels? ")) * 0.05
     pennies = int(input("How many pennies? ")) * 0.01
     total = quarters + dimes + nickels + pennies
-
     return total
+
+def is_transaction_successful(coins_inserted, drink_cost):
+    """Return true when the payment is accepted, or false if money is insufficient"""
+    if coins_inserted >= drink_cost:
+        change = coins_inserted - drink_cost
+        if change > 0:
+            print(f"Here is your change: ${change:.2f}")
+        return True
+    else:
+        print("Sorry, not enough money!")
+        return False
 
 while True:
     choice = input("What would you like? (espresso/latte/cappuccino) ").lower()
@@ -76,12 +89,7 @@ while True:
         if check_resources(choice):
             print(f"Insert coins: ${MENU[choice]["cost"]}")
             payment = process_coins()
-            if payment >= MENU[choice]["cost"]:
-                change = payment - MENU[choice]["cost"]
-                if change > 0:
-                    print(f"Here is your change: ${change:.2f}")
+            if is_transaction_successful(payment, MENU[choice]["cost"]):
                 make_drink(choice)
-            else:
-                print("Sorry, not enough money!")
     else:
         print("Invalid input!")
